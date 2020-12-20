1a43fa8c24cd8c9e6d5c19a6348f1170e084027b
Sandra <lissetdovah@gmail.com>
2020-12-20 17:13:57 -0600

    funcVariadicas-Practice 1.1

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+var figure = make([]string, 0)
+var otherFigure = make([]string, 0)
+
 func main() {
 	calcArea(0, 0)
 	calcArea(10, 10, "Square")
 	calcArea(15, 20, "Triangle", "Rectangle", "Trapezium")
+
+	figure = append(figure, "Square", "Triangle", "Rectangle", "Trapezium")
+	otherFigure = append(otherFigure, figure...)
+	otherFigure = append(otherFigure, "circle", "hexagon")
+
+	calcArea(0, 0, otherFigure...)
+	fmt.Println(otherFigure)
+
 }
 
 func calcArea(width, height int, figure ...string) {
